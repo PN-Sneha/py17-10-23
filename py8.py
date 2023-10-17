23a358a5be4c7200297f3a1f390a64a32fc4be0c
PN-Sneha <125186963+PN-Sneha@users.noreply.github.com>
2023-10-17 23:36:42 +0530

    Add files via upload

---
+++ py8.py
@@ -0,0 +1,13 @@
+def printSquares(n):
+    square = 0
+    odd = 1
+    for x in range(0 , n):
+         
+        print(square, end= " ")
+          
+        square = square + odd
+        odd = odd + 2
+
+n = int(input());
+printSquares(n)
+
